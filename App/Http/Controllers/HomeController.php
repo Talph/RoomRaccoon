ec1ad88631c97804bb9616ecb9febf139d323622
Tafara Shamu <evasion-barques0s@icloud.com>
2023-04-01 14:38:08 +0200

    [feat] basic routing and controllers

---
+++ App/Http/Controllers/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+
+use Core\Http\Controller;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function notFound()
+    {
+
+    }
+}
